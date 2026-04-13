7ae1ab1bdcf9a6a3f14b4e8431f663c3602ed5bb
Anbelin <huihuang_yw@foxmail.com>
2026-04-13 18:16:03 +0800

    invoke llm in stream

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ def main():
     # 初始化 ChatOllama，指定你运行的模型名称
     llm = ChatOllama(
         model="deepseek-r1:latest",  # 与 ollama run 后的名称保持一致
-        temperature=0.7,  # 可选：控制输出随机性
+        temperature=0,  # 可选：控制输出随机性
         num_predict=512,  # 可选：最大生成 token 数
     )
 
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+        ),
+        ("human", "I love programming."),
+    ]
+
     # 发送消息并获取回复
-    response = llm.invoke("你好，请介绍一下你自己")
+    response = llm.stream(messages)
 
     # 打印回复内容
-    print(response.content)
+    for chunk in response:
+        print(chunk.content, end="")
 
 
 if __name__ == "__main__":
